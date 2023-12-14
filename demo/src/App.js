a7e7f772c60f9b58f7f9fc0637e281c7c4ac216d
SV-5 <153350843+SV-5@users.noreply.github.com>
2023-12-14 15:34:58 +0530

    Add files via upload

---
+++ demo/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './assets/css/App.css';
+import LoginPage from './components/LoginPage';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage/>
+    </div>
+  );
+}
+export default App;
